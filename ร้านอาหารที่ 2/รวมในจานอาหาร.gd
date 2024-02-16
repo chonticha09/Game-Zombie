5f982f3bf3c1582e2036812beb508e495e0a8f9c
ProjectZombiecooking <projectjob12345@gmail.com>
2024-02-16 19:55:05 +0700

    อัพ

--- ร้านอาหารที่ 2/รวมในจานอาหาร.gd
+++ ร้านอาหารที่ 2/รวมในจานอาหาร.gd
@@ -10,6 +10,10 @@ func _ready():
 		get_node("จานเปล่า1/จาน1/เลม่อน2"),
 		get_node("จานเปล่า3/จาน3/เลม่อน3"),
 		get_node("จานเปล่า3/จาน3/เลม่อน4"),
+		get_node("จานเปล่า4/จาน4/มะเขือเทศ1"),
+		get_node("จานเปล่า4/จาน4/มะเขือเทศ2"),
+		get_node("จานเปล่า2/จาน2/ผัก1"),
+		get_node("จานเปล่า3/จาน3/ผัก2"),
 		get_node("จานเปล่า1/จาน1/ซอส1"),
 		get_node("จานเปล่า2/จาน2/ซอส2")
 	]
@@ -27,11 +31,26 @@ func load_brain_sauce_texture(texture_rect: TextureRect) -> void:
 	var texture2 = preload("res://ร้านอาหารที่ 2/ร้านอาหารทะเลใหม่/น้ำจิ้มหัวสมอง.png")
 	texture_rect.texture = texture2
 
+# ฟังก์ชันโหลดรูปภาพมะเขือเทศ
+func load_มะเขือเทศ_texture(texture_rect: TextureRect) -> void:
+	var texture3 = preload("res://ร้านอาหารที่ 2/ร้านอาหารทะเลใหม่/มะเขื่อเทศ.png")
+	texture_rect.texture = texture3
+	
+func load_ผัก_texture(texture_rect: TextureRect) -> void:
+	var texture4 = preload("res://ร้านอาหารที่ 2/ร้านอาหารทะเลใหม่/ผัก.png")
+	texture_rect.texture = texture4
+
 # เมื่อปุ่มถูกกด (โหลดรูปภาพเลม่อน)
 func _on_ถาดมะนาว_pressed():
 	show_next_mango_texture()
-
-# เมื่อปุ่มถูกกด (โหลดรูปภาพน้ำจิ้มหัวสมอง)
+	
+# เมื่อปุ่มถูกกด (โหลดรูปภาพมะเขือเทศ)
+func _on_ถาดมะเขือเทศ_pressed():
+	show_next_มะเขือเทศ_texture()
+	
+func _on_ถาดผักกาด_pressed():
+	show_next_ผัก_texture()
+	
 func _on_ถาดซอส_pressed():
 	show_next_brain_sauce_texture()
 
@@ -44,6 +63,25 @@ func show_next_mango_texture() -> void:
 	else:
 		print("All TextureRects have been shown.")
 
+		
+func show_next_มะเขือเทศ_texture() -> void:
+	if current_texture_index < texture_rect_nodes.size():
+		var current_texture_rect = texture_rect_nodes[current_texture_index]
+		load_มะเขือเทศ_texture(current_texture_rect)
+		current_texture_rect.visible = true
+		current_texture_index += 1
+	else:
+		print("All TextureRects have been shown.")
+		
+func show_next_ผัก_texture() -> void:
+	if current_texture_index < texture_rect_nodes.size():
+		var current_texture_rect = texture_rect_nodes[current_texture_index]
+		load_ผัก_texture(current_texture_rect)
+		current_texture_rect.visible = true
+		current_texture_index += 1
+	else:
+		print("All TextureRects have been shown.")
+		
 func show_next_brain_sauce_texture() -> void:
 	if current_texture_index < texture_rect_nodes.size():
 		var current_texture_rect = texture_rect_nodes[current_texture_index]
