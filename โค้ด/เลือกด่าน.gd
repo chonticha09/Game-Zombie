2c5c1f963ef67f573398c567335176a914e8f240
ProjectZombiecooking <154109832+ProjectZombiecooking@users.noreply.github.com>
2024-03-08 14:07:38 +0700

    แก้หน้าเลือกด่าน

--- โค้ด/เลือกด่าน.gd
+++ โค้ด/เลือกด่าน.gd
@@ -1,47 +1,60 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func _on_texture_button_pressed():
-	get_tree().change_scene_to_file("res://ซีนต่างๆ/node_2d.tscn")
 	
 func _on_texture_button_mouse_entered():
+	get_tree().change_scene_to_file("res://ซีนต่างๆ/node_2d.tscn")
 	$Sound1.play()
 	pass # Replace with function body.
+	
 func _on_texture_button_2_mouse_entered():
+	get_tree().change_scene_to_file("res://ซีนต่างๆ/node_2d.tscn")
 	$Sound1.play()
 	pass # Replace with function body.
+	
 func _on_texture_button_3_mouse_entered():
+	get_tree().change_scene_to_file("res://ซีนต่างๆ/node_2d.tscn")
 	$Sound1.play()
 	pass # Replace with function body.
+	
 func _on_texture_button_5_mouse_entered():
+	get_tree().change_scene_to_file("res://ซีนต่างๆ/node_2d.tscn")
 	$Sound1.play()
 	pass # Replace with function body.
+	
 func _on_texture_button_6_mouse_entered():
+	get_tree().change_scene_to_file("res://ซีนต่างๆ/node_2d.tscn")
 	$Sound1.play()
 	pass # Replace with function body.
+	
 func _on_texture_button_7_mouse_entered():
+	get_tree().change_scene_to_file("res://ซีนต่างๆ/node_2d.tscn")
 	$Sound1.play()
 	pass # Replace with function body.
+	
 func _on_texture_button_8_mouse_entered():
+	get_tree().change_scene_to_file("res://ซีนต่างๆ/node_2d.tscn")
 	$Sound1.play()
 	pass # Replace with function body.
+	
 func _on_texture_button_4_mouse_entered():
+	get_tree().change_scene_to_file("res://ซีนต่างๆ/node_2d.tscn")
 	$Sound1.play()
 	pass # Replace with function body.
+	
 func _on_texture_button_9_mouse_entered():
+	get_tree().change_scene_to_file("res://ซีนต่างๆ/node_2d.tscn")
 	$Sound1.play()
 	pass # Replace with function body.
+	
 func _on_texture_button_10_mouse_entered():
+	get_tree().change_scene_to_file("res://ซีนต่างๆ/node_2d.tscn")
 	$Sound1.play()
 	pass # Replace with function body.
 
