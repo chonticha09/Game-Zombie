2f822ade62d0f0194bb9e93689cc702018619207
ProjectZombiecooking <154109832+ProjectZombiecooking@users.noreply.github.com>
2024-03-11 23:10:08 +0700

    แก้ทุกหน้า

--- โค้ด/Menu.gd
+++ โค้ด/Menu.gd
@@ -1,15 +1,34 @@
 extends Control
 
+var texture_rect_node: TextureRect
+var current_texture_index: int = 0
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# หา TextureRect ในฉากและเก็บไว้
+	texture_rect_node = get_node("เซ็ทติ้ง")
+	
+	# ทำให้ TextureRect ไม่แสดง
+	hide_texture_rect()
 
+func load_texture(path: String, texture_rect: TextureRect) -> void:
+	# โหลดรูปภาพจาก path และกำหนดให้ TextureRect
+	var texture = preload("res://เพิ่มเติม/หน้าตั้งค่า.png")
+	texture_rect.texture = texture
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
+# เมื่อปุ่มถูกกด
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://ซีนต่างๆ/option.tscn")	
+	# ถ้า TextureRect กำลังแสดง
+	if texture_rect_node.visible:
+		# ทำให้ TextureRect ไม่แสดง
+		hide_texture_rect()
+	else:
+		# โหลดและแสดงรูปภาพ
+		load_texture("res://เพิ่มเติม/หน้าตั้งค่า.png", texture_rect_node)
+		texture_rect_node.visible = true
+		current_texture_index += 1
+		
+	
+func hide_texture_rect():
+	# ทำให้ TextureRect ไม่แสดง
+	texture_rect_node.visible = false
+
