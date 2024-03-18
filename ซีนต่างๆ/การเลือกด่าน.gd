5d623114db71f49463736b1d101069685ebf07cc
ProjectZombiecooking <154109832+ProjectZombiecooking@users.noreply.github.com>
2024-03-18 23:22:13 +0700

    เพิ่มโค้ดหน้าเลือกด่าน

---
+++ ซีนต่างๆ/การเลือกด่าน.gd
@@ -0,0 +1,69 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_1_pressed():
+	get_tree().change_scene_to_file("res://ร้านอาหารที่ 2/ร้านอาหารทะเล.tscn")
+	$Sound1.play()
+
+
+
+func _on_ด่านที่1_pressed():
+	pass # Replace with function body.
+
+
+func _on_ด่านที่2_pressed():
+	pass # Replace with function body.
+
+
+func _on_ด่านที3_pressed():
+	pass # Replace with function body.
+
+
+func _on_ด่านที4_pressed():
+	pass # Replace with function body.
+
+
+func _on_ด่านที5_pressed():
+	pass # Replace with function body.
+
+
+func _on_ด่านที6_pressed():
+	pass # Replace with function body.
+
+
+func _on_ด่านที7_pressed():
+	pass # Replace with function body.
+
+
+func _on_ด่านที8_pressed():
+	pass # Replace with function body.
+
+
+func _on_ด่านที9_pressed():
+	pass # Replace with function body.
+
+
+func _on_ด่านที10_pressed():
+	pass # Replace with function body.
+
+
+func _on_ด่านที11_pressed():
+	pass # Replace with function body.
+
+
+func _on_ด่านที12_pressed():
+	pass # Replace with function body.
+
+
+func _on_ด่านที13_pressed():
+	pass # Replace with function body.
