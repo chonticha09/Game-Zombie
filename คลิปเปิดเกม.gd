4617975d8ad4be31625ff362de1591bdb58e7d92
ProjectZombiecooking <154109832+ProjectZombiecooking@users.noreply.github.com>
2024-03-19 21:34:59 +0700

    แก้ไขหน้าเปิดเกม

---
+++ คลิปเปิดเกม.gd
@@ -0,0 +1,29 @@
+extends Control
+
+# ตรวจสอบหากว่าวิดีโอเคยเล่นเสร็จหรือยัง
+var video_finished: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Replace with function body.
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# Replace with function body.
+	pass
+
+# ถูกเรียกทุกครั้งเมื่อวิดีโอเล่นเสร็จ
+func _on_video_stream_player_finished():
+	# ตั้งค่าให้วิดีโอเล่นเสร็จ
+	video_finished = true
+	
+	# เรียกฟังก์ชันเพื่อเปลี่ยนฉากหรือ Screen
+	change_scene_to_next()
+
+# ฟังก์ชันที่ใช้เปลี่ยนฉากหรือ Screen
+func change_scene_to_next():
+	# ตรวจสอบว่าวิดีโอเล่นเสร็จแล้วหรือไม่
+	if video_finished:
+		# โหลดหน้าจอหรือฉากต่อไป
+		get_tree().change_scene_to_file("res://ซีนต่างๆ/main.tscn")
