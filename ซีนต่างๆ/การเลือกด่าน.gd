33ad9998e11c18d82d68b2f765cb3a50df0b9a65
ProjectZombiecooking <154109832+ProjectZombiecooking@users.noreply.github.com>
2024-03-19 21:04:10 +0700

    แก้เสียง

---
+++ ซีนต่างๆ/การเลือกด่าน.gd
@@ -0,0 +1,102 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+
+func _on_ด่านที่1_pressed():
+	get_tree().change_scene_to_file("res://ซีนต่างๆ/ร้านอาหารจีน.tscn")
+func _on_ด่านที่1_mouse_entered():
+	$S1.play()
+
+
+func _on_ด่านที่2_pressed():
+	get_tree().change_scene_to_file("res://ซีนต่างๆ/ร้านอาหารจีน.tscn")
+func _on_ด่านที่2_mouse_entered():
+	$S1.play()
+
+
+func _on_ด่านที3_pressed():
+	get_tree().change_scene_to_file("res://ซีนต่างๆ/ร้านอาหารจีน.tscn")
+func _on_ด่านที3_mouse_entered():
+	$S1.play()
+
+
+func _on_ด่านที4_pressed():
+	get_tree().change_scene_to_file("res://ซีนต่างๆ/ร้านอาหารจีน.tscn")
+func _on_ด่านที4_mouse_entered():
+	$S1.play()
+
+
+func _on_ด่านที5_pressed():
+	get_tree().change_scene_to_file("res://ซีนต่างๆ/ร้านอาหารจีน.tscn")
+func _on_ด่านที5_mouse_entered():
+	$S1.play()
+
+
+func _on_ด่านที6_pressed():
+	get_tree().change_scene_to_file("res://ร้านอาหารที่ 2/ร้านอาหารทะเล.tscn")
+func _on_ด่านที6_mouse_entered():
+	$S1.play()
+
+
+func _on_ด่านที7_pressed():
+	get_tree().change_scene_to_file("res://ร้านอาหารที่ 2/ร้านอาหารทะเล.tscn")
+func _on_ด่านที7_mouse_entered():
+	$S1.play()
+
+
+func _on_ด่านที8_pressed():
+	get_tree().change_scene_to_file("res://ร้านอาหารที่ 2/ร้านอาหารทะเล.tscn")
+func _on_ด่านที8_mouse_entered():
+	$S1.play()
+
+
+func _on_ด่านที9_pressed():
+	get_tree().change_scene_to_file("res://ร้านอาหารที่ 2/ร้านอาหารทะเล.tscn")
+func _on_ด่านที9_mouse_entered():
+	$S1.play()
+
+
+func _on_ด่านที10_pressed():
+	get_tree().change_scene_to_file("res://ร้านอาหารที่ 2/ร้านอาหารทะเล.tscn")
+func _on_ด่านที10_mouse_entered():
+	$S1.play()
+
+
+func _on_ด่านที11_pressed():
+	get_tree().change_scene_to_file("res://ร้านอาหารญี่ปุ่น/ร้านอาหารญี่ปุ่น.tscn")
+func _on_ด่านที11_mouse_entered():
+	$S1.play()
+
+
+func _on_ด่านที12_pressed():
+	get_tree().change_scene_to_file("res://ร้านอาหารญี่ปุ่น/ร้านอาหารญี่ปุ่น.tscn")
+func _on_ด่านที12_mouse_entered():
+	$S1.play()
+
+
+func _on_ด่านที13_pressed():
+	get_tree().change_scene_to_file("res://ร้านอาหารญี่ปุ่น/ร้านอาหารญี่ปุ่น.tscn")
+func _on_ด่านที13_mouse_entered():
+	$S1.play()
+
+
+func _on_ด่านที14_pressed():
+	get_tree().change_scene_to_file("res://ร้านอาหารญี่ปุ่น/ร้านอาหารญี่ปุ่น.tscn")
+func _on_ด่านที14_mouse_entered():
+	$S1.play()
+
+
+func _on_ด่านที15_pressed():
+	get_tree().change_scene_to_file("res://ร้านอาหารญี่ปุ่น/ร้านอาหารญี่ปุ่น.tscn")
+func _on_ด่านที15_mouse_entered():
+	$S1.play()
+
+
+
