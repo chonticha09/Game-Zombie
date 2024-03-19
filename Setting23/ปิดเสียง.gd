c1b5862954e078b93e563f7b175cb63c64530e4d
ProjectZombiecooking <154109832+ProjectZombiecooking@users.noreply.github.com>
2024-03-20 02:43:49 +0700

    แก้ไขเปิดปิดเสียง
    เสร็จแล้ว

--- Setting23/ปิดเสียง.gd
+++ Setting23/ปิดเสียง.gd
@@ -1,6 +1,7 @@
 extends TextureButton
 
 var audio_enabled = true
+var is_clicked = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,3 +20,11 @@ func _on_pressed():
 	else:
 		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), false)
 		audio_enabled = true
+
+	# เปลี่ยนรูปภาพของ TextureButton เมื่อถูกคลิก
+	if is_clicked:
+		texture_normal = load("res://เพิ่มเติม/Sound(1)_0.png")
+	else:
+		texture_normal = load("res://เพิ่มเติม/Sound(2)_0.png")
+
+	is_clicked = not is_clicked
