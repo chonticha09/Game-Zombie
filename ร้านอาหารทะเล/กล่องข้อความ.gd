d578ff800ac1ae437ca9c11cc4f13823aae3e6cc
ProjectZombiecooking <154109832+ProjectZombiecooking@users.noreply.github.com>
2024-03-31 04:49:13 +0700

    แก้ไขสุ่มอาหาร

--- ร้านอาหารทะเล/กล่องข้อความ.gd
+++ ร้านอาหารทะเล/กล่องข้อความ.gd
@@ -1,11 +1,43 @@
 extends TextureRect
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	# Replace with function body.
+	# Call the function to randomly set image from TextureRect.
+	randomize_image()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+# Function to randomly set image from TextureRect.
+func randomize_image():
+	# Get the total number of textures in the TextureRect.
+	var num_textures = get_texture_count()
+	
+	# Check if there are textures available.
+	if num_textures > 0:
+		# Generate a random index within the range of available textures.
+		var random_index = randi() % num_textures
+		
+		# Set the texture based on the randomly generated index.
+		set_texture(get_texture(random_index))
+	else:
+		print("No textures available in TextureRect!")
+
+# Function to get the total number of textures in the TextureRect.
+func get_texture_count() -> int:
+	return get_child_count()
+
+# Function to get the texture at a specific index from the TextureRect.
+func get_texture(index: int) -> Texture:
+	# Get the child node at the specified index.
+	var child_node = get_child(index)
+	
+	# Check if the child node is a TextureRect.
+	if child_node is TextureRect:
+		# Return the texture of the TextureRect.
+		return child_node.texture
+	else:
+		print("Child node at index", index, "is not a TextureRect!")
+		return null
--- ร้านอาหารทะเล/สุ่มอาหาร.gd
+++ ร้านอาหารทะเล/สุ่มอาหาร.gd
@@ -1,11 +1,34 @@
 extends Control
 
+# List of image paths for seafood items
+var seafood_images: Array = [
+	"res://ร้านอาหารทะเล/ร้านอาหารทะเลใหม่/เมนูอาหารทะเล/จานผักซอสปลา.png",
+	"res://ร้านอาหารทะเล/ร้านอาหารทะเลใหม่/เมนูอาหารทะเล/จานผักมะนาวซอสปลา.png",
+	"res://ร้านอาหารทะเล/ร้านอาหารทะเลใหม่/เมนูอาหารทะเล/จานเนื้อผักมะเขือ.png"
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# Replace with function body.
+	# Randomize and set image from the list of seafood images
+	randomize_seafood_image()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+# Function to randomize and set image from the list of seafood images
+func randomize_seafood_image() -> void:
+	# Check if there are images in the list
+	if seafood_images.size() > 0:
+		# Generate a random index within the range of available images
+		var random_index = randi() % seafood_images.size()
+		
+		# Load the image from the randomly selected path
+		var image_texture = load(seafood_images[random_index])
+		
+		# Set the loaded texture to the TextureRect
+		if image_texture != null:
+			# Assuming there's a TextureRect node named "TextureRect" under this Control node
+			var texture_rect = $TextureRect
+			texture_rect.texture = image_texture
+		else:
+			print("Failed to load image from path:", seafood_images[random_index])
+	else:
+		print("No seafood images available!")
