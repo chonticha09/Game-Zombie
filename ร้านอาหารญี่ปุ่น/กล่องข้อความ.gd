811bc8aab123bb92033efe7446c0b32d7a3013e8
ProjectZombiecooking <154109832+ProjectZombiecooking@users.noreply.github.com>
2024-03-30 10:02:52 +0700

    แก้สุ่ม

--- ร้านอาหารญี่ปุ่น/กล่องข้อความ.gd
+++ ร้านอาหารญี่ปุ่น/กล่องข้อความ.gd
@@ -1,10 +1,23 @@
 extends TextureRect
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var random_food_node = Control.new()  # สร้างโหนด Control ใหม่
-	random_food_node.name = "สุ่มอาหาร"  # กำหนดชื่อให้กับโหนด Control
-	add_child(random_food_node)  # เพิ่มโหนด Control เข้าไปในโหนด TextureRect
+var textures = []  # อาร์เรย์เก็บรูปภาพที่ใช้สำหรับสุ่ม
 
-	# เรียกใช้งานฟังก์ชันหรือเมทอดของคลาส Control
-	random_food_node.some_control_function()  
+func _ready():
+	# เตรียมรูปภาพที่จะใช้สำหรับสุ่ม
+	textures.append(preload("res://ร้านอาหารญี่ปุ่น/สุ่มอาหาร/ซุซิทูน่า.png"))
+	textures.append(preload("res://ร้านอาหารญี่ปุ่น/สุ่มอาหาร/ซุซิแซลม่อน.png"))
+	textures.append(preload("res://ร้านอาหารญี่ปุ่น/สุ่มอาหาร/ซูซิกุ้ง.png"))
+	textures.append(preload("res://ร้านอาหารญี่ปุ่น/สุ่มอาหาร/ซูซิไข่หวาน.png"))
+	# เลือกรูปภาพแบบสุ่มเป็นรูปเริ่มต้น
+	set_random_texture()
+
+func set_random_texture():
+	# ตรวจสอบว่าอาร์เรย์ textures มีข้อมูลอยู่หรือไม่
+	if textures.size() > 0:
+		# เลือกรูปภาพสุ่มจากอาร์เรย์ของรูปภาพ
+		var random_index = randi_range(0, textures.size() - 1)
+		var selected_texture = textures[random_index]
+		# กำหนดรูปภาพให้กับ TextureRect
+		set_texture(selected_texture)
+	else:
+		print("Error: textures array is empty.")
--- ร้านอาหารทะเล/มันม่าวง.gd
+++ ร้านอาหารทะเล/มันม่าวง.gd
@@ -1,7 +1,8 @@
 extends Sprite2D
 
-var speed = 25  # ความเร็วในการเคลื่อนที่
+var speed = 150  # ความเร็วในการเคลื่อนที่
 var moving = true  # สถานะการเคลื่อนที่ของ Sprite
+var textures = []  # อาร์เรย์เก็บรูปภาพที่ใช้สำหรับสุ่ม
 
 func _process(delta):
 	if moving:
@@ -17,6 +18,28 @@ func _process(delta):
 func _ready():
 	# โหลดรูปภาพของ Sprite
 	var texture = preload("res://กล่องข้อความ.png")
+
+	# เตรียมรูปภาพที่จะใช้สำหรับสุ่ม
+	textures.append(preload("res://ร้านอาหารญี่ปุ่น/สุ่มอาหาร/ซุซิทูน่า.png"))
+	textures.append(preload("res://ร้านอาหารญี่ปุ่น/สุ่มอาหาร/ซุซิแซลม่อน.png"))
+	textures.append(preload("res://ร้านอาหารญี่ปุ่น/สุ่มอาหาร/ซูซิกุ้ง.png"))
+	textures.append(preload("res://ร้านอาหารญี่ปุ่น/สุ่มอาหาร/ซูซิไข่หวาน.png"))
+
+	# เลือกรูปภาพแบบสุ่มเป็นรูปเริ่มต้น
+	set_random_texture()
+
 	# กำหนดรูปภาพให้กับ Sprite
 	texture = preload("res://ร้านพิซซ่า/มันม่าวง.png")
 	set_texture(texture)
+
+func set_random_texture():
+	# ตรวจสอบว่าอาร์เรย์ textures มีข้อมูลอยู่หรือไม่
+	if textures.size() > 0:
+		# เลือกรูปภาพสุ่มจากอาร์เรย์ของรูปภาพ
+		var random_index = randi_range(0, textures.size() - 1)
+		var selected_texture = textures[random_index]
+		# กำหนดรูปภาพให้กับ Sprite
+		texture = selected_texture
+		set_texture(texture)
+	else:
+		print("Error: textures array is empty.")
