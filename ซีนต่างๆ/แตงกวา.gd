56d1597ee7ca015683c0e54c3c5f838c1b780aa7
ProjectZombiecooking <154109832+ProjectZombiecooking@users.noreply.github.com>
2024-04-01 17:44:31 +0700

    แก้หน้าร้านอาหารจีน

---
+++ ซีนต่างๆ/แตงกวา.gd
@@ -0,0 +1,39 @@
+extends TextureButton
+
+
+var texture_rect_nodes: Array
+var current_texture_index: int = 0
+
+func _ready():
+	# Find TextureRects in the scene and store them in an array
+	texture_rect_nodes = [
+		get_node("แตงกวา1"),
+		get_node("แตงกวา2")
+	]
+	# Hide all TextureRects
+	hide_all_texture_rects()
+
+# Function to load mango texture
+func load_กุ้ง_texture(texture_rect: TextureRect) -> void:
+	var texture1 = preload("res://อาหารจีน/ต้นหอม.png")
+	texture_rect.texture = texture1
+
+# When the button is pressed (load mango texture)
+func _on_pressed():
+	$เสียงกด.play()
+	show_next_กุ้ง_texture()
+
+func show_next_กุ้ง_texture() -> void:
+	if current_texture_index < texture_rect_nodes.size():
+		var current_texture_rect = texture_rect_nodes[current_texture_index]
+		load_กุ้ง_texture(current_texture_rect)
+		current_texture_rect.visible = true
+		current_texture_index += 1
+	else:
+		print("All TextureRects have been shown.")
+
+func hide_all_texture_rects():
+	for texture_rect in texture_rect_nodes:
+		texture_rect.visible = false
+
+
