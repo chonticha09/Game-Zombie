21fefb3285aa82cfd8788abb88999b761bbcf9ae
ProjectZombiecooking <154109832+ProjectZombiecooking@users.noreply.github.com>
2024-04-01 17:22:16 +0700

    แก้หน้าเพิ่มเสียงลดเสียง

---
+++ ร้านอาหารทะเล/CheckButton.gd
@@ -0,0 +1,8 @@
+extends CheckButton
+
+
+func _on_toggled(button_pressed):
+	if button_pressed == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
